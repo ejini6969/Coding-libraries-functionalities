90914d45a2578a85c279a4df91363111d3b29a7c
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-12-09 22:53:44 +0800

    Update ascii.js

--- javascript/ascii.js
+++ javascript/ascii.js
@@ -1,3 +1,7 @@
 function sumAscii(s){
   return [...s].reduce((a, b) => a + b.charCodeAt(), 0)
-}
+} // s = "abc" etc.
+
+function fromAscii(arr){
+  return arr.map(x => String.fromCharCode(x)).join``
+} // arr = [65, 122, 42, 97] etc.
