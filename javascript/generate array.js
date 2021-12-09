11c64754280f717f180764106c6e63cca21e98d2
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-12-09 11:41:33 +0800

    Update generate array.js

--- javascript/generate array.js
+++ javascript/generate array.js
@@ -1,3 +1,14 @@
 function generate(n){
   return Array.from({length: n}, (_, i) => i + 1) // start from 1 to n
 }
+
+function range(m, n){
+  // assuming n >= m
+  return Array.from({length: n - m + 1}, (_, i) => i + m) // start from m to n
+}
+
+function rangeNoOrder(m, n){
+  m = Math.min(m, n)
+  n = Math.max(m, n)
+  return Array.from({length: n - m + 1}, (_, i) => i + m) // start from m to n (after redefining variable)
+}
