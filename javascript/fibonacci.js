88cb97fbd65a478b257a4c91764ccf761f6e3a16
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2022-01-21 09:54:07 +0800

    Update fibonacci.js

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,3 +1,14 @@
 function fib(n){
   return n < 2 ? n : fib(~-n) + fib(n - 2)
 }
+
+const _ = require("bignumber.js")
+
+function fib2(n){ // Big int
+  let a = new _(0)
+  let b = new _(1)
+  while(n--){
+    [a, b] = [b, a.plus(b)]
+  }
+  return a.toFixed(0)
+}
