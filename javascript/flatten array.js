453eb540bb2fe72d56f31e0c6262b9f2662e5119
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2022-01-21 09:39:12 +0800

    Create flatten array.js

---
+++ javascript/flatten array.js
@@ -0,0 +1,40 @@
+// 2-dimenstional arrays (array of arrays)
+function flatten1(arr){
+  return [].concat(...arr)
+}
+
+function flatten2(arr){
+  return Array.prototype.concat(...arr)
+}
+
+function flatten3(arr){
+  return arr.reduce((a, b) => a.concat(b), [])
+}
+
+// multi-dimensional arrays (nested arrats)
+function nestedflatten1(arr){
+  return arr.reduce((a, b) => a.concat(Array.isArray(b) ? nestedflatten1(...b) : b), [])
+}
+
+const _ = require("lodash")
+
+function nestedflatten2(arr) {
+  return _.flattenDeep(arr);
+}
+
+const 战 = require("ramda")
+
+function nestedflatten3(arr) {
+  return 战.flatten(arr);
+}
+
+function nestedflatten4(arr){
+  let ans = []
+  for(let x of arr){
+    if(Array.isArray(x))
+      ans.push(...nestedflatten4(...x))
+     else
+       ans.push(x)
+  }
+  return ans
+}
